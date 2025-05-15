027aeed3949ce71abbe9e61e9e7aa1077e2c644e
SKXNIX <artur.antushev196@mail.ru>
2025-05-15 06:29:33 +0300

    Доработан модуль управления учетными записями пользователей

--- backend/apps/administrator/urls.py
+++ backend/apps/administrator/urls.py
@@ -9,8 +9,11 @@ urlpatterns = [
     path('content/', views.content, name='content'),    # Доп страница
     path('settings/', views.settings, name='settings'), # Доп страница
 
+    path('delete-selected-users/', views.DeleteSelectedUsersView.as_view(), name='delete-selected-users'),
+
     path('users/', views.UserListView.as_view(), name='users'),                                                 # Список пользователей
     path('users/create/', views.UserCreateView.as_view(), name='user-create'),                                  # Создание пользователя
+    path('users/import-users/', views.ImportUsersView.as_view(), name='import-users'),                          # Импорт пользователей
     path('users/<int:pk>/update/', views.UserUpdateView.as_view(), name='user-update'),                         # Изменение пользователя
     path('users/<int:pk>/delete/', views.UserDeleteView.as_view(), name='user-delete'),                         # Удаление пользователя
     path('users/<int:pk>/reset-password/', views.PasswordResetView.as_view(), name='user-reset-password'),      # Сброс пароля
--- backend/apps/custom_auth/models.py
+++ backend/apps/custom_auth/models.py
@@ -13,9 +13,11 @@ class CustomUser(AbstractUser):
         ('admin', 'Админ'),
         ('storekeeper', 'Кладовщик'),
     )
-    full_name = models.CharField(max_length=255, blank=True, null=True)
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='storekeeper')
-    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+    username = models.CharField(verbose_name='Логин', max_length=150, unique=True)
+    password = models.CharField(verbose_name='Пароль', max_length=128)
+    full_name = models.CharField(verbose_name='ФИО', max_length=255, blank=True, null=True)
+    role = models.CharField(verbose_name='Должность', max_length=20, choices=ROLE_CHOICES, default='storekeeper')
+    avatar = models.ImageField(verbose_name='Аватар', upload_to='avatars/', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
